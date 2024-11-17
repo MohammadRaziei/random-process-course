e707712f83eba9696e43a6d49ef48084714e19b2
MohammadRaziei <mohammadraziei1375@gmail.com>
2024-11-17 22:52:45 +0330

    add all

--- Q1/main.m
+++ Q1/main.m
@@ -1,207 +1,129 @@
-clc; close all; clear
-mkdir results
-addpath ../common/
+clc; close all; clear;
 
-rng(1)
+% Create a directory for results
+mkdir('results');
+addpath('../common/');
 
-sigma2 = 1;
+% Set random seed for reproducibility
+rng(1);
 
-h = [1 -.8 .6];
+% Constants
+n_molecules = 1000;        % Number of molecules
+s_m = 500;                 % Molecule speed (arbitrary units)
+T = 1e-9;                  % Time step between movements
+t = 1e-4;                  % Total time for each molecule's movement
+n_movements = floor(t / T); % Number of movements per molecule
 
-Rxx_idx = 0;
-Rxx = sigma2;
+% Generate random angles for each molecule's movement
+phi = rand(n_molecules, n_movements) * 2 * pi;  % Azimuthal angle
+theta = rand(n_molecules, n_movements) * pi;    % Polar angle
 
-Rxy = conv(conj(fliplr(h)), Rxx);
-Ryy = conv(h, Rxy);
+% Convert spherical coordinates to Cartesian coordinates
+[dx, dy, dz] = sph2cart(phi, pi/2 - theta, s_m * T);
 
-figure
-Rxy_idx = (1-length(h)):0;
-stem(Rxy_idx, Rxy)
-xlim([-length(h), 1])
-ylim([-2, 3])
-for i = 1:length(Rxy)
-    text(Rxy_idx(i), Rxy(i)+.3*sign(Rxy(i)), sprintf("%g", Rxy(i)), "HorizontalAlignment", "center");
+% Initialize positions
+x = [zeros(n_molecules, 1), cumsum(dx, 2)];
+y = [zeros(n_molecules, 1), cumsum(dy, 2)];
+z = [zeros(n_molecules, 1), cumsum(dz, 2)];
+
+% Calculate the average positions (x, y, z)
+steps = 0:n_movements;
+mean_x = mean(x, 1);
+mean_y = mean(y, 1);
+mean_z = mean(z, 1);
+
+saver = [];
+saver =  [saver; ["mean_x", mean_x(end)]; ["mean_y", mean_y(end)]; ["mean_z", mean_z(end)]];
+%% Plot average positions over time
+figure('units', 'normalized', 'outerposition', [0 .25 1 .5]); 
+axis off;
+[subplot_axis, ~] = tight_subplot(1, 1, [0.2, 0.05], .2);
+ax = subplot_axis(1);
+axes(ax); 
+hold on;
+plot(steps, mean_x, 'DisplayName', 'Mean X');
+plot(steps, mean_y, 'DisplayName', 'Mean Y');
+plot(steps, mean_z, 'DisplayName', 'Mean Z');
+legend;
+exportgraphics(gcf, 'results/mean_positions.pdf', 'Append', false);
+
+
+%% 3D plot of the average position
+figure;
+ax = gca;
+ax.View = [-25, 25];
+ax.DataAspectRatio = [1 1 1];
+plot3(mean_x, mean_y, mean_z);
+
+exportgraphics(gcf, 'results/3d_average_trajectory.pdf', 'Append', false);
+
+%% Plot individual trajectories of the first few molecules
+figure; 
+hold on;
+ax = gca;
+ax.View = [-25, 25];
+ax.DataAspectRatio = [1 1 1];
+
+for i = 1:min(n_molecules, 10)
+    plot3(x(i, :), y(i, :), z(i, :));
 end
-exportgraphics(gcf, 'results/Rxy.pdf', 'Append', false);
+exportgraphics(gcf, 'results/individual_trajectories.pdf', 'Append', false);
 
-figure
-Ryy_idx = (1-length(h)):(length(h)-1);
-stem(Ryy_idx, Ryy)
-xlim([-length(h), length(h)])
-ylim([-2, 3])
-for i = 1:length(Ryy)
-    text(Ryy_idx(i), Ryy(i)+.3*sign(Ryy(i)), sprintf("%g", Ryy(i)), "HorizontalAlignment", "center");
-end
-exportgraphics(gcf, 'results/Ryy.pdf', 'Append', false);
+%% Calculate and plot the mean squared displacement
+mean_d = mean(sqrt(x.^2 + y.^2 + z.^2), 1);
+
+figure('units', 'normalized', 'outerposition', [0 .25 1 .5]); 
+axis off;
+[subplot_axis, ~] = tight_subplot(1, 1, [0.2, 0.05], .2);
+ax = subplot_axis(1);
+axes(ax); 
+hold on;
+plot(steps, mean_d);
+exportgraphics(gcf, 'results/mean_displacement.pdf', 'Append', false);
+
+saver =  [saver; ["mean_d", mean_d(end)]];
+
+%% Calculate and plot the root mean squared displacement 
+rms = sqrt(mean(x.^2 + y.^2 + z.^2, 1));
+
+figure('units', 'normalized', 'outerposition', [0 .25 1 .5]); 
+axis off;
+[subplot_axis, ~] = tight_subplot(1, 1, [0.2, 0.05], .2);
+ax = subplot_axis(1);
+axes(ax); 
+hold on;
+plot(steps, rms);
+exportgraphics(gcf, 'results/root_mean_squared.pdf', 'Append', false);
+saver =  [saver; ["rms", rms(end)]];
 
 
 
 %%
 
+d = sqrt(mean_x(end)^2 + mean_y(end)^2 + mean_z(end)^2);
+t_4m_d = 4 / d * t;
+disp(t_4m_d)
+
+saver =  [saver; ["d", d]];
 
 
-% Number of samples (length of the process)
-for N = [100, 10000]
+d = mean_d(end);
+t_4m_md = 4 / d * t;
+disp(t_4m_md)
 
-% Process parameters
-mean_value = 0;      % Mean
-variance_value = 1;  % Variance
 
-% Generate white Gaussian noise process
-white_noise_process = sqrt(variance_value) * randn(N, 1) + mean_value;
-
-% Plot the generated process
-figure;
-plot(white_noise_process);
-title('White Gaussian Noise Process (x[n])');
-xlabel('Sample Number');
-ylabel('Amplitude');
-exportgraphics(gcf, sprintf('results/white-noise-N%i.pdf', N), 'Append', false);
-
-% Convolution to obtain y[n]
-x = white_noise_process;
-y = conv(x, h, "full");
-y = y(1:length(x));
-
-figure
-plot(y);
-xlabel('Sample Number');
-ylabel('Amplitude');
-title('y[n]');
-exportgraphics(gcf, sprintf('results/yn-N%i.pdf', N), 'Append', false);
+d = rms(end);
+t_4m_rms = 4 / d * t;
+disp(t_4m_rms)
 
 
 
-% Display the distribution of the process as a histogram
-figure;
-histogram(white_noise_process, 50);
-title('Histogram of White Gaussian Noise Process');
-xlabel('Value');
-ylabel('Frequency');
-exportgraphics(gcf, sprintf('results/white-noise-hist-N%i.pdf', N), 'Append', false);
+saver =  [saver; ["t_4m_d", t_4m_d]];
+saver =  [saver; ["t_4m_md", t_4m_md]];
+saver =  [saver; ["t_4m_rms", t_4m_rms]];
 
+writematrix(saver, "results/saver.csv")
 
-
-% Autocorrelation of x[n]
-R_x = xcorr(x, 'biased');
-
-% Autocorrelation of y[n]
-R_y = xcorr(y, 'biased');
-
-% Cross-correlation between x[n] and y[n]
-R_xy = xcorr(x, y, 'biased');
-
-% Plotting
-figure('units','normalized','outerposition',[0 .25 1 .75]);
-ax = gca;
-outerpos = ax.OuterPosition;
-ti = ax.TightInset; 
-left = outerpos(1) + 2.2*ti(1);
-bottom = outerpos(2) + 2.2*ti(2);
-ax_width = outerpos(3) - 2.2*ti(1) - ti(3);
-ax_height = outerpos(4) - 2.2*ti(2) - ti(4);
-ax.Position = [left bottom ax_width ax_height];
-axis off
-[subplot_axis, pos] = tight_subplot(3, 2, [0.15, 0.05], .1); 
-
-axes(subplot_axis(1)); axis on; hold on
-plot(-N+1:N-1, R_x);
-title('Autocorrelation of x[n]');
-xlabel('Lag (n)');
-ylabel('Amplitude');
-
-axes(subplot_axis(2)); axis on; hold on
-plot(-N+1:N-1, R_x);
-title('Autocorrelation of x[n]');
-xlabel('Lag (n)');
-ylabel('Amplitude');
-stem(Rxx_idx, Rxx)
-xlim([-10,10])
-
-axes(subplot_axis(3)); axis on; hold on
-plot(-N+1:N-1, R_y);
-title('Autocorrelation of y[n]');
-xlabel('Lag (n)');
-ylabel('Amplitude');
-
-axes(subplot_axis(4)); axis on; hold on
-plot(-N+1:N-1, R_y);
-title('Autocorrelation of y[n]');
-xlabel('Lag (n)');
-ylabel('Amplitude');
-stem(Ryy_idx, Ryy)
-xlim([-10,10])
-
-axes(subplot_axis(5)); axis on; hold on
-plot(-N+1:N-1, R_xy);
-title('Cross-correlation between x[n] and y[n]');
-xlabel('Lag (n)');
-ylabel('Amplitude');
-
-axes(subplot_axis(6)); axis on; hold on
-plot(-N+1:N-1, R_xy);
-title('Cross-correlation between x[n] and y[n]');
-xlabel('Lag (n)');
-ylabel('Amplitude');
-stem(Rxy_idx, Rxy)
-xlim([-10,10])
-exportgraphics(gcf, sprintf('results/xcorr-N%i.pdf', N), 'Append', false);
-
-
-% Theoretical Power Spectral Density of x[n]
-S_xx = variance_value * ones(N, 1);  % Constant value for white noise
-
-[H, w] = freqz(h, 1, N, 'whole');
-
-
-% Calculate the Power Spectral Density of x[n] using Welch's method
-window_length = max(floor(N/16), 25);     % Window length for Welch's method
-overlap = floor(window_length/2);  % 50% overlap
-
-
-[PSD_x, f] = pwelch(x, window_length, overlap, N, 1, 'centered');
-PSD_x = fftshift(PSD_x); f = f + .5;
-fftx = fft(x); Sx = abs(fftx).^2 / N;
-
-% Plotting the PSD
-figure('units','normalized','outerposition',[0 .25 1 .75]); axis off
-[subplot_axis, pos] = tight_subplot(2, 1, [0.2, 0.05], .1);
-
-axes(subplot_axis(1)); axis on; hold on
-plot(w/(2*pi), Sx, "Color", .8*ones(1,3));
-plot(f, PSD_x);
-plot(w/(2*pi), S_xx, "LineWidth", 1, "Color", "blue");
-ylim([0, max(PSD_x)*1.5])
-
-
-title('Power Spectral Density of x[n]');
-xlabel('Normalized Frequency (rad/sample)');
-ylabel('S_{xx}(f)');
-
-% Theoretical Power Spectral Density of y[n]
-S_yy = abs(H).^2 * variance_value;
-
-% Calculate the Power Spectral Density of x[n] using Welch's method
-[PSD_y, f] = pwelch(y, window_length, overlap, N, 1, 'centered');
-PSD_y = fftshift(PSD_y); f = f + .5;
-ffty = fft(y); Sy = abs(ffty).^2 / N;
-
-
-
-axes(subplot_axis(2)); axis on; hold on
-plot(w/(2*pi), Sy, "Color", .8*ones(1,3));
-plot(f, PSD_y);
-plot(w/(2*pi), S_yy, "LineWidth", 1, "Color", "blue");
-title('Power Spectral Density of y[n]');
-xlabel('Normalized Frequency (rad/sample)');
-ylabel('S_{yy}(f)');
-ylim([0, max(PSD_y)*1.5])
-
-exportgraphics(gcf, sprintf('results/psd-N%i.pdf', N), 'Append', false);
-
-
-end
-
-
+%%
 
 
--- Q2/main.m
+++ Q2/main.m
@@ -49,17 +49,18 @@ ax = subplot_axis(1);
 axes(ax)
 
 stairs(t, M);
+ax.Box = 'off';
+
 xlabel("Minutes")
 ylabel("M(t)")
 
 
 exportgraphics(gcf, sprintf('results/mt-%d.pdf', T), 'Append', false);
-
 %%
 rng(1);
 
 lambda1 = 10;
-lambda2 = 0.1;
+lambda2 = 0.5;
 
 
 num_iter = 500;
@@ -74,14 +75,60 @@ Ms = Ms / num_iter;
 
 % Exponential fit function
 fitFunc = @(b, x) b(1) * (1 - exp(-x * b(2)));  
-% 
-% % Initial guess for parameters
-% initialGuess = [max(Ms), 10]; 
-% 
-% % Fit the model using fminsearch
-% params = fminsearch(@(b) sum((Ms - fitFunc(b, t)).^2), initialGuess);
+% Initial guess for parameters
+initialGuess = [max(Ms), 1]; 
+% Fit the model using fminsearch
+params = fminsearch(@(b) sum((Ms - fitFunc(b, t)).^2), initialGuess);
 
+
+
+
+figure('units','normalized','outerposition',[0 .25 1 .5]); axis off
+[subplot_axis, ~] = tight_subplot(1, 1, [0.2, 0.05], .2);
+ax = subplot_axis(1);
+axes(ax); hold on
+
+plot(t, Ms, 'b');
+
+plot(t, fitFunc(params, t), 'r:');
+
+xlabel("t (Minutes)")
+ylabel("M(t)")
+
+legend({'$\mathbf{E}\big\{M(t)\big\}$', ...
+    sprintf('$%.5g(1 - e^{-%.5g t})$', params(1), params(2))}, ...
+    'Interpreter', 'latex', 'Location','southeast');
+
+csvwrite(sprintf('results/e-mt-lambda1-%.2f-lambda2-%.2f-fit.csv', lambda1, lambda2), params);
+
+exportgraphics(gcf, sprintf('results/e-mt-lambda1-%.2f-lambda2-%.2f-fit.pdf', lambda1, lambda2), 'Append', false);
+
+
+%%
+rng(1);
+
+lambda1 = 10;
+lambda2 = 0.5;
+
+
+num_iter = 500;
+Ms = 0;
+for i = 1:num_iter
+    [t, M] = Mt(poisson_points_process(lambda1, T), lambda2, T);
+    Ms = Ms + M;
+end
+Ms = Ms / num_iter;
+
+
+
+% Exponential fit function
+fitFunc = @(b, x) b(1) * (1 - exp(-x * b(2)));  
 params = [lambda1 / lambda2, lambda2];
+
+initialGuess = [max(Ms), 1]; 
+% Fit the model using fminsearch
+params_fit = fminsearch(@(b) sum((Ms - fitFunc(b, t)).^2), initialGuess);
+
 
 
 
@@ -94,7 +141,7 @@ plot(t, Ms, 'b');
 
 plot(t, fitFunc(params, t), 'k:');
 
-xlabel("Minutes")
+xlabel("t (Minutes)")
 ylabel("M(t)")
 title(sprintf("$\\lambda_1: %.5g \\qquad \\lambda_2: %.5g$", lambda1, lambda2), "Interpreter", "latex")
 
@@ -102,6 +149,60 @@ legend({'$\mathbf{E}\big\{M(t)\big\}$', ...
     sprintf('$\\frac{\\lambda_1}{\\lambda_2} (1 - e^{-\\lambda_2 t}) = %.5g(1 - e^{-%.5g t})$', params(1), params(2))}, ...
     'Interpreter', 'latex', 'Location','southeast');
 
-
+csvwrite(sprintf('results/e-mt-lambda1-%.2f-lambda2-%.2f.csv', lambda1, lambda2), [params_fit; params]);
 
 exportgraphics(gcf, sprintf('results/e-mt-lambda1-%.2f-lambda2-%.2f.pdf', lambda1, lambda2), 'Append', false);
+
+%%
+
+rng(2)
+
+TT = 3*T;
+lambda1 = 10;
+lambda2 = 0.5;
+
+Tt = (0:.1:TT)';
+MTt = zeros(length(Tt), 1);
+for i = 1:length(Tt)
+[t, M] = Mt(poisson_points_process(lambda1, Tt(i)), lambda2, Tt(i));
+MTt(i) = mean(M);
+end
+
+% Exponential fit function
+fitFunc = @(b, x) b(1) * (1 - exp(-x * b(2)));  
+% Initial guess for parameters
+initialGuess = [max(MTt), 1]; 
+% Fit the model using fminsearch
+params_fit = fminsearch(@(b) sum((MTt - fitFunc(b, Tt)).^2), initialGuess);
+
+
+params = [lambda1 / lambda2, lambda2 / 2];
+
+
+figure('units','normalized','outerposition',[0 .25 1 .5]); axis off
+[subplot_axis, ~] = tight_subplot(1, 1, [0.2, 0.05], .2);
+ax = subplot_axis(1);
+axes(ax); hold on
+
+plot(Tt, MTt)
+plot(Tt, fitFunc(params_fit, Tt), 'r:');
+plot(t, fitFunc(params, t), 'k:');
+
+xlabel("T (Minutes)")
+ylabel("M(t)")
+
+title(sprintf("$\\lambda_1: %.5g \\qquad \\lambda_2: %.5g$", lambda1, lambda2), "Interpreter", "latex")
+
+
+legend({'$<M(t)>$', ...
+    sprintf('$%.5g(1 - e^{-%.5g t})$', params_fit(1), params_fit(2)), ...
+    sprintf('$\\frac{\\lambda_1}{\\lambda_2} (1 - e^{-\\frac{\\lambda_2}{2}t}) = %.5g(1 - e^{-%.5g t})$', params(1), params(2))}, ...
+    'Interpreter', 'latex', 'Location','southeast');
+
+
+csvwrite(sprintf('results/e-mt-lambda1-%.2f-lambda2-%.2f-time.csv', lambda1, lambda2), [params_fit; params])
+
+
+exportgraphics(gcf, sprintf('results/e-mt-lambda1-%.2f-lambda2-%.2f-time.pdf', lambda1, lambda2), 'Append', false);
+
+
